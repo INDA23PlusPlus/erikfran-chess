5a0ca44905570abe68521560133c3c743c055712
ErikFrankling <erik.frankling@frankling.se>
2023-09-11 16:46:06 +0200

    move validation logic and testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //TODO: think about if Copy and Clone are necessary just because compiler recommends it
 #[derive(Copy, Clone)]
 pub enum PieceTypes {
-    Pawn,
+    Pawn(bool),
     Bishop,
     Knight,
     Rook,
@@ -17,8 +17,8 @@ pub enum Color {
 
 #[derive(Copy, Clone)]
 pub struct Piece {
-    piece: PieceTypes,
-    color: Color,
+    pub piece: PieceTypes,
+    pub color: Color,
 }
 
 ///A chess game. All the data from the game is accessible in the fields of the struct but should only be mutated through the associated methods.
@@ -30,6 +30,7 @@ pub struct Game {
     pub castling_white: bool,
     pub fifty_move_rule: u8,
     pub en_passant: Option<(usize, usize)>,
+    pub check: bool,
 }
 
 impl Game {
@@ -46,12 +47,12 @@ impl Game {
                     Some(Piece { piece: PieceTypes::Knight, color: Color::White }),
                     Some(Piece { piece: PieceTypes::Rook, color: Color::White })
                 ],
-                [Some(Piece { piece: PieceTypes::Pawn, color: Color::White }); 8],
+                [Some(Piece { piece: PieceTypes::Pawn(false), color: Color::White }); 8],
                 [None; 8],
                 [None; 8],
                 [None; 8],
                 [None; 8],
-                [Some(Piece { piece: PieceTypes::Pawn, color: Color::Black }); 8],
+                [Some(Piece { piece: PieceTypes::Pawn(false), color: Color::Black }); 8],
                 [
                     Some(Piece { piece: PieceTypes::Rook, color: Color::Black }), 
                     Some(Piece { piece: PieceTypes::Knight, color: Color::Black }), 
@@ -69,19 +70,20 @@ impl Game {
             castling_white: true,
             fifty_move_rule: 0,
             en_passant: None,
+            check: false,
         }
     }
 
     ///Tries making move described by coordinates. If unsuccessful no move will be made and you can try making a leagal move again. The return vale is an option that shows if a player has won this turn and in that case which color won.
     pub fn try_move(&mut self, x_origin: usize, y_origin: usize, x_destination: usize, y_destination: usize) -> Result<Option<Color>, MoveError> {
-        let origin = match self.board[x_origin][y_origin] {
+        let origin = match self.board[y_origin][x_origin] {
             Some( Piece { color: c, .. } ) if c != self.turn => 
             return Err(MoveError::OpponentPiece("".into())),
             Some(o) => o,
             None => return Err(MoveError::EmptyOriginSquare("".into())),
         };
 
-        let captured = match self.board[x_destination][y_destination] {
+        let captured = match self.board[y_destination][x_destination] {
             Some(Piece { color: c, .. }) if c == origin.color => 
             return Err(MoveError::SelfCollision("".into())),
             destination @ _ => destination
@@ -90,42 +92,110 @@ impl Game {
         //TODO: return errors if moves collide, are not possible for the specific piece or lead to check.
         match origin.piece {
             PieceTypes::Bishop => {
-                
+                if x_origin.abs_diff(x_destination) != y_origin.abs_diff(y_destination) {
+                    return Err(MoveError::WrongPieceMovement);
+                }
+                else if self.collision_check_diagonal(x_origin, y_origin, x_destination, y_destination) {
+                    return Err(MoveError::Collision);
+                }
             },
             PieceTypes::King => {
-
+                if !((x_origin).abs_diff(x_destination) <= 1 && (y_origin).abs_diff(y_destination) <= 1) {
+                    return Err(MoveError::WrongPieceMovement);
+                }
+                //casteling
             },
             PieceTypes::Queen => {
-                
+                if x_origin.abs_diff(x_destination) == y_origin.abs_diff(y_destination)  {
+                    if self.collision_check_diagonal(x_origin, y_origin, x_destination, y_destination) {
+                        return Err(MoveError::Collision);
+                    }
+                }
+                else if (x_origin.abs_diff(x_destination) > 0) ^ (y_origin.abs_diff(y_destination) > 0) {
+                    if self.collision_check_straight(x_origin, y_origin, x_destination, y_destination) {
+                        return Err(MoveError::Collision);
+                    }
+                }
+                else {
+                    return Err(MoveError::WrongPieceMovement);
+                }
             },
             PieceTypes::Rook => {
-                
+                if !((x_origin.abs_diff(x_destination) > 0) ^ (y_origin.abs_diff(y_destination) > 0)){
+                    return Err(MoveError::WrongPieceMovement);
+                }
+                else if self.collision_check_straight(x_origin, y_origin, x_destination, y_destination) {
+                    return Err(MoveError::Collision);
+                }
+                if match self.turn {
+                    Color::Black => self.castling_black,
+                    Color::White => self.castling_white
+                } {
+                    //TODO add castling
+                }
             },
-            PieceTypes::Pawn => {
-                
+            PieceTypes::Pawn(moved) => {
+                let origin_row: i32;
+                let multiply: i32 = match self.turn {
+                    Color::White => { origin_row = 1; 1 },
+                    Color::Black => { origin_row = 6; -1 }
+                };
+
+                if !(multiply * (y_destination - y_origin) as i32 <= 2) {
+                    return Err(MoveError::WrongPieceMovement);
+                }
+                //checks for en passant
+                else if y_destination.abs_diff(y_origin) == 2 {
+                    if moved { return Err(MoveError::PawnDubbleMove); }
+                    if self.collision_check_straight(x_origin, y_origin, x_destination, y_destination) {
+                        return Err(MoveError::Collision);
+                    }
+                    else if y_origin as i32 == origin_row {
+                        self.en_passant = Some((x_destination, y_destination));
+                        //TODO: add en passant
+                    }
+                }
+                //checks for promotions
+                if origin_row - multiply == y_destination as i32 {
+                    //TODO: add promotions 
+                    //IDEA: use a game state enum to indicate when promotion happens.
+                }
+                self.board[y_origin][x_origin].unwrap().piece = PieceTypes::Pawn(true)
             },
             PieceTypes::Knight => {
-                
+                if !(((x_origin.abs_diff(x_destination) == 2) && (y_origin.abs_diff(y_destination) == 1)) ^ ((x_origin.abs_diff(x_destination) == 1) && (y_origin.abs_diff(y_destination) == 2))) {
+                    return Err(MoveError::WrongPieceMovement);
+                }
             },
         }
 
-        self.board[x_destination][y_destination] = Some(origin);
-        self.board[x_origin][y_origin] = None;
+        self.board[y_destination][x_destination] = Some(origin);
+        self.board[y_origin][x_origin] = None;
 
         if let Some(c) = captured {
             self.captured.push(c);
         }
 
-        //TODO: Check for checkmate
+        //TODO: Check for checkmate and checks
+        //TODO: fifty move rule
+        //TODO: make draw possible
 
         Ok(None)
+    }
+
+    fn collision_check_straight(&self, x_origin: usize, y_origin: usize, x_destination: usize, y_destination: usize) -> bool {
+        false
+    }
+
+    fn collision_check_diagonal(&self, x_origin: usize, y_origin: usize, x_destination: usize, y_destination: usize) -> bool {
+        false
     }
 
     pub fn possible_moves(x: usize, y: usize) -> [[bool; 8]; 8] {
         todo!()
     }
 
-    pub fn translate_game_to_chessnotation(&self) -> &str {
+    pub fn translate_game_to_pgn(&self) -> &str {
         todo!()
     }
 
@@ -137,7 +207,7 @@ impl Game {
         todo!()
     }
 
-    pub fn translate_game_from_chessnotation(input: &str) -> Game {
+    pub fn translate_game_from_pgn(input: &str) -> Game {
         todo!()
     }
 }
@@ -148,5 +218,105 @@ pub enum MoveError {
     SelfCollision(String),
     OpponentPiece(String),
     EmptyOriginSquare(String),
-    WrongPieceMovment(String),
+    WrongPieceMovement,
+    Collision,
+    PawnDubbleMove
+}
+
+//TODO: add tests
+#[cfg(test)]
+mod tests {
+    use crate::{ Game, MoveError, Color, Piece, PieceTypes };
+
+    #[test]
+    fn it_works() -> Result<(), MoveError> {
+        let move_list = [
+                [0, 1, 0, 2], //Pawn
+                [0, 0, 0, 1], //Rook
+                [1, 0, 2, 2], //Knight
+                [2, 0, 4, 2], //bishop
+                [3, 0, 2, 0], //Queen
+                [4, 0, 3, 0], //King
+                //[3, 0, 3, 0], //King
+            ];
+
+        let mut game = Game::new();
+
+        let mut r: Result<(), MoveError> = Ok(());
+        
+        for row in move_list {
+            let x_origin = row[0];
+            let y_origin = row[1];
+            let x_destination = row[2];
+            let y_destination = row[3];
+            
+            println!("moving {}, from {x_origin}, {y_origin} to {x_destination}, {y_destination}", match game.board[y_origin][x_origin] {
+                Some(Piece { piece: PieceTypes::Bishop, .. }) => "b",
+                Some(Piece { piece: PieceTypes::Rook, .. }) => "r",
+                Some(Piece { piece: PieceTypes::King, .. }) => "K",
+                Some(Piece { piece: PieceTypes::Queen, .. }) => "q",
+                Some(Piece { piece: PieceTypes::Knight, .. }) => "k",
+                Some(Piece { piece: PieceTypes::Pawn(_), .. }) => "p",
+                None => ".",
+            });
+            r = match game.try_move(x_origin, y_origin, x_destination, y_destination) {
+                Ok(win) => { 
+                    println!("moved {}", match game.board[y_destination][x_destination] {
+                        Some(Piece { piece: PieceTypes::Bishop, .. }) => "b",
+                        Some(Piece { piece: PieceTypes::Rook, .. }) => "r",
+                        Some(Piece { piece: PieceTypes::King, .. }) => "K",
+                        Some(Piece { piece: PieceTypes::Queen, .. }) => "q",
+                        Some(Piece { piece: PieceTypes::Knight, .. }) => "k",
+                        Some(Piece { piece: PieceTypes::Pawn(_), .. }) => "p",
+                        None => ".",
+                    });
+                    
+                    if let Some(color) = win{
+                        let winner = match color {
+                            Color::Black => "black",
+                            Color::White => "white"
+                        };
+                        println!("moved {}, {} to {}, {} and {} won the game", x_origin, y_origin, x_destination, y_destination, winner);
+                    }
+                    else {
+                        println!("moved {}, {} to {}, {}", x_origin, y_origin, x_destination, y_destination);
+                    }
+                    Ok(())
+                },
+                Err(error) => { return Err(error); }
+            };
+            
+            let mut y_cord: usize = 0;
+            for y in game.board {
+                let mut t = String::new();
+                let mut x_cord: usize = 0;
+                for x in y {
+                    /*if x_cord == x_origin && y_cord == y_origin {
+                        t += "*";
+                        x_cord += 1;
+                        continue;
+                    }
+                    if x_cord == x_destination && y_cord == y_destination {
+                        t += "#";
+                        x_cord += 1;
+                        continue;
+                    }*/
+                    
+                    t += match x {
+                        Some(Piece { piece: PieceTypes::Bishop, .. }) => "b",
+                        Some(Piece { piece: PieceTypes::Rook, .. }) => "r",
+                        Some(Piece { piece: PieceTypes::King, .. }) => "K",
+                        Some(Piece { piece: PieceTypes::Queen, .. }) => "q",
+                        Some(Piece { piece: PieceTypes::Knight, .. }) => "k",
+                        Some(Piece { piece: PieceTypes::Pawn(_), .. }) => "p",
+                        None => ".",
+                    };
+                    x_cord += 1;
+                }
+                y_cord += 1;
+                println!("{}",t);
+            }
+        }
+        r
+    }
 }
